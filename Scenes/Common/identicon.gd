837415e925a43a896b2f9f4926d1aefe306d1560
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2026-08-12 01:24:01 -0400

    identicons remade
    shader no more

--- Scenes/Common/identicon.gd
+++ Scenes/Common/identicon.gd
@@ -1,16 +1,66 @@
-extends ColorRect
+extends Control
 class_name Identicon
 
-var Num: int = 0:
+var _col: Color = Color.WHITE:
+	get:
+		return Main.GetCallsignColor(Num)
+
+@export var Num: int = 0:
 	set(value):
 		Num = value
-		color = Main.GetCallsignColor(Num)
-		material.set_shader_parameter("Value", Num)
+		queue_redraw()
+
+@export var Size: int = 16:
+	set(value):
+		Size = value
+		custom_minimum_size.x = Size
+		custom_minimum_size.y = custom_minimum_size.x
+		custom_maximum_size = custom_minimum_size
+		queue_redraw()
 
 func _ready():
-	Main.instance.ReloadSettings.connect(Refresh)
+	Main.instance.ReloadSettings.connect(refresh)
 
-func Refresh():
-	custom_minimum_size.x = roundi(16.0 * SettingsHandler.FontSize / 18.0)
-	custom_minimum_size.y = custom_minimum_size.x
-	custom_maximum_size = custom_minimum_size
+func refresh():
+	Size = roundi(16.0 * SettingsHandler.FontSize / 18.0)
+
+func _draw():
+	var subsize: float = Size * 0.2
+	for i: int in range(15):
+		var x: int = i % 3
+		var y: int = floori(i / 3.0)
+		var r: int = Num
+		if r % 2 == 0:
+			y = 4 - y
+		if (r % 3) == 0:
+			x = 2 - x
+		y = (r % 5 + y) % 5
+		
+		var idx: int = x + y * 3
+		if y == 2:
+			idx += 6
+		if y > 2:
+			idx -= 3
+		var bigSkip: bool = false
+		for j: int in range(12):
+			if r % 2 == 0 and idx == j:
+				bigSkip = true
+				break
+			r = floori(r * 0.5)
+		if bigSkip:
+			continue
+		if (idx == 12) and (Num % 7) == 1:
+			continue
+		if (idx == 13) and (Num % 11) == 1:
+			continue
+		if (idx == 14) and (Num % 13) == 1:
+			continue
+		draw_rect(Rect2(
+			Vector2((2 - i % 3) * subsize, floori(i / 3.0) * subsize),
+			Vector2(subsize, subsize)),
+		_col)
+		if i % 3 != 0:
+			draw_rect(Rect2(
+				Vector2((2 + i % 3) * subsize, floori(i / 3.0) * subsize),
+				Vector2(subsize, subsize)),
+			_col)
