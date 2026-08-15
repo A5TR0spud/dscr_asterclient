3af684b744fe679d6f3082af78fef3389d2035d6
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2026-08-15 03:02:11 -0400

    Update parse_result.gd
    fix types

--- Scripts/parse_result.gd
+++ Scripts/parse_result.gd
@@ -15,8 +15,8 @@ enum FailureState {
 ## Current state of the result, i.e., whether it contains strings or not
 var state: FailureState = FailureState.UNPARSED
 ## Array holding parsed integers
-var output: PackedInt32Array = PackedInt32Array([])
+var output: Array[int] = [] as Array[int]
 ## Array of indices of the provided string that halted the parser, if the parsing failed to a(n) unknown string(s)
-var stopping_indices: PackedInt32Array = PackedInt32Array([])
+var stopping_indices: Array[int] = [] as Array[int]
 ## Array of failed strings
-var failures: PackedStringArray = PackedStringArray([])
+var failures: Array[String] = [] as Array[String]
