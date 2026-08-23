05a5cbbb8ee99c64abc025dce2fb78e91c9d3932
NikZapp <dev.nikzapp@gmail.com>
2026-08-23 19:06:48 +0500

    Revert "custom autocomplete"
    This reverts commit dc56c7555ed413de36345e177b1fb322be94f035.

--- Scenes/ChatMenu/message_edit.gd
+++ Scenes/ChatMenu/message_edit.gd
@@ -1,13 +1,17 @@
 extends TextEdit
 
-@onready var autocomplete_list: ItemList = $PanelContainer/MarginContainer/ItemList
+@onready var autocomplete_list: ItemList = $PanelContainer/ItemList
 @onready var auto_list_panel: PanelContainer = $PanelContainer
+#CodeEdit hates trying to autocomplete things without spaces, so use a nested one which contains only the word to try
+@onready var autocomplete_finder: CodeEdit = $AutocompleteFinder
+
 const DELIMITER_CHARACTER = "\u001f"
 
 func _ready():
 	Main.instance.reload_dict.connect(refresh)
 	Main.instance.reload_settings.connect(_set_popup_size.call_deferred)
 	auto_list_panel.hide()
+	autocomplete_finder.hide()
 
 func refresh():
 	placeholder_text = DictionaryHandler.signals_to_words([-43, -38])
@@ -36,7 +40,18 @@ func _request_code_completion(force: bool) -> void:
 		auto_list_panel.hide()
 		return
 	
-	var options: Array[String] = AutocompleteManager.get_candidates(word_under_caret)
+	#print("CHECKING: ", word_under_caret)
+	
+	# TODO: make this better
+	# dont add every word?
+	# but only if it makes it lag
+	autocomplete_finder.text = word_under_caret
+	autocomplete_finder.set_caret_column(word_under_caret.length() + 1)
+	for word: String in (DictionaryHandler.word_names as Array[String]):
+		autocomplete_finder.add_code_completion_option(CodeEdit.KIND_PLAIN_TEXT, word, word)
+	autocomplete_finder.update_code_completion_options(true)
+
+	var options: Array[Dictionary] = autocomplete_finder.get_code_completion_options()
 	if options.is_empty():
 		#print("NO OPTIONS FOR ", word_under_caret)
 		auto_list_panel.hide()
@@ -50,6 +65,7 @@ func _request_code_completion(force: bool) -> void:
 				return
 		insert_text(DELIMITER_CHARACTER, get_caret_line(), bounds[1])
 		return
+	autocomplete_finder.cancel_code_completion()
 	
 	_show_custom_popup(options)
 
@@ -80,10 +96,10 @@ func get_signal_under_caret() -> String:
 	var bounds = get_signal_bounds_under_caret()
 	return line_text.substr(bounds[0], bounds[1] - bounds[0])
 
-func _show_custom_popup(options: Array[String]):
+func _show_custom_popup(options: Array[Dictionary]):
 	autocomplete_list.clear()
 	for option in options:
-		autocomplete_list.add_item(option)
+		autocomplete_list.add_item(option["display_text"])
 	autocomplete_list.select(0)
 	
 	var word = get_signal_under_caret()
